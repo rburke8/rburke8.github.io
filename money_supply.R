4554275a353285a616d83e48e47e390faacd451b
Your Name <you@example.com>
2026-03-29 22:10:27 +0100

    moneysupply

--- money_supply.R
+++ money_supply.R
@@ -1,28 +1,36 @@
 #money supply A2.2.1 - Components of M4 https://www.bankofengland.co.uk/statistics/Tables
 library(plm)
 library(lmtest)
+library(dynlm)
 library(dplyr)
+library(vars)
+library(sandwich)
 library(tidyr)
 library(stargazer)
 library(ggplot2)
+library(lubridate)
+library(quantmod)
 library(tidyverse)
+library(stringr)
 library(scales)
 library(ggthemes)
 ##Retail deposits and cash, M4 - excluding wholsesale 
 
 df <- read.csv("data/Bank of England  Database (3).csv") %>% 
-  rename(M4 = 2) %>% 
-  mutate(Date = as.Date(Date, format = "%d %b %y")) %>% 
-  arrange(Date)
-
+  rename(M4 = 2)%>%
+  mutate(Date = as.Date(Date, format = "%d %b %y"),
+         Date = floor_date(Date, "month"))
+  
 #growth
 df_growth <- df %>% 
   mutate(
+    basic = (M4 - lag(M4)) / lag(M4),
     mom = 100 * (M4 - lag(M4)) / lag(M4),
     growth_3m_ann = ((M4 / lag(M4, 3))^4 - 1) * 100,
     growth_6m_ann = ((M4 / lag(M4, 6))^2 - 1) * 100,
-    growth_yoy = 100 * (M4 - lag(M4, 12)) / lag(M4, 12)
+    money_growth_yoy = 100 * (M4 - lag(M4, 12)) / lag(M4, 12)
   )
+
 
 
 #----
@@ -55,4 +63,63 @@ ggplot(df_growth, aes(x = Date, y = growth_3m_ann)) +
   theme(panel.background = element_rect("black")) +
   theme(plot.background = element_rect("black"))
 
+#----
+#Inflation
+inflationn <- read.csv("data/series-290326.csv") %>% 
+  slice(193:638) %>% 
+  rename(inflation = 2,
+         Date = Title) %>% 
+  mutate(
+    # 1. Parse "1989 JAN" → Date
+    Date = as.Date(paste0(Date, " 01"), format = "%Y %b %d"),
+    
+    # 2. Strip day → first of month
+    Date = floor_date(Date, "month")
+  ) %>% 
+  left_join(df_growth, by = "Date") %>% 
+  mutate(inflation = as.numeric(inflation))
 
+ggplot(data = inflation, aes(x = Date, y = inflation)) +
+  geom_line(color = "yellow", linewidth = 0.5) +
+  labs(x = "Date", y = "Inflation") +
+  ggtitle("Annual Inflation (%)") +
+  theme(plot.title = element_text(face = "bold", size = 9))+
+  theme(axis.title.x = element_text(color = "white"))+
+  theme(axis.title.y = element_text(color = "white"))+
+  theme(panel.background = element_rect("black")) +
+  theme(plot.background = element_rect("black"))+
+  theme(plot.title = element_text(color = "white"))
+
+#----
+#time series regs
+inflationn <- as.data.frame(inflationn)
+df2 <- inflationn %>% 
+  select(1)
+
+money_inf <- ts(df2, 
+                start = c(1989,1),
+                end = c(2025,1),
+                frequency = 12)
+#Get optimal lag length
+lagselect <- VARselect(money_inf, lag.max = 12, type = "const")
+lagselect
+model1 <- dynlm(inflation ~ L(money_growth_yoy),
+                data = money_inf, 
+                start = c(1989,1), 
+                end = c(2025,1))
+model2 <- dynlm(inflation ~ L(money_growth_yoy, 1:12) + L(inflation, 1:12),
+                data = money_inf, 
+                start = c(1989,1), 
+                end = c(2025,1))
+
+
+coeftest(model2, vcov. = NeweyWest(model2))
+
+bgtest(model2, order = 18)
+
+var12 <- VAR(money_inf, p = 12, type = "const")
+serial.test(var12, lags.pt = 18, type = "PT.asymptotic")
+model_seasonal <- dynlm(
+  inflation ~ L(inflation, 1:4) + L(money_growth_yoy, 1:4) + season(Date),
+  data = inflation
+)
--- money_supply.md
+++ money_supply.md
@@ -11,19 +11,29 @@ deposits, I use total retail deposits for households and private
 non-financial corporations. Wholesale deposits and repos are excluded,
 as well as notes and coin.
 
-The data runs from 1982-06-30 to 2026-01-31. I plot total money supply
+The data runs from 1982-06-01 to 2026-01-01. I plot total money supply
 in sterling millions, month-on-month % growth rate as well as 3-month
 annualised growth rate.
 
 ![](money_supply_files/figure-gfm/plots-1.png)<!-- -->
 
-The money supply as of 2026-01-31 is £2,358,688,000,000, i.e. £2
+The money supply as of 2026-01-01 is £2,358,688,000,000, i.e. £2
 trillion.
 
 <img src="money_supply_files/figure-gfm/more-plots-1.png" width="50%" /><img src="money_supply_files/figure-gfm/more-plots-2.png" width="50%" />
 
 For month-on-month growth rates, the highest growth occurred on
-1992-12-31, with a growth rate of 7.6%.
+2008-09-01, with a growth rate of 2.19%.
 
 For 3-month annualised growth rates, the highest growth occurred on
-1982-12-31, with a growth rate of 36.65%.
+2015-12-01, with a growth rate of 7.8%.
+
+## Inflation
+
+I plot annualised inflation data from
+<https://www.ons.gov.uk/economy/inflationandpriceindices/timeseries/l55o/mm23>.
+
+![](money_supply_files/figure-gfm/inflation-plot-1.png)<!-- -->
+
+Between 1989-01-01 and 2026-02-01, the highest recorded rate of
+inflation was 9.6% which occured in 2022-10-01
