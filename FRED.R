c899db230a10a034c8d382abb21e4be02474cbdd
Your Name <you@example.com>
2026-03-30 22:35:57 +0100

    as

---
+++ FRED.R
@@ -0,0 +1,140 @@
+#----
+#setup
+#https://fredblog.stlouisfed.org/2024/12/leveraging-r-for-powerful-data-analysis/
+library(dplyr)
+library(tidyverse)
+library(tidyr)
+library(ggplot2)
+library(fredo)
+library(jsonlite)
+
+
+api_key <- Sys.getenv("FRED_API_KEY")
+
+#----
+#fetching some data
+# Define the series IDs and date range
+
+#----
+#us bank deposits
+series_ids <- c("DPSACBW027SBOG")
+start_date <- "1973-01-03"
+end_date <- "2026-03-18"
+
+
+df <- fredo(api_key, series_ids, start_date, end_date)
+
+ggplot(df, aes(x = date, y = value)) + 
+  geom_line(color = "yellow", linewidth = 1) +
+  labs(
+    x = "Year",
+    y = "Billions of Dollars",
+    title = "All deposits, commercial banks"
+  ) +
+  scale_y_continuous(labels = scales::comma) +
+  theme(
+    plot.title = element_text(size = 9, face = "bold", colour = "white"),
+    panel.background = element_rect(fill = "black"),
+    plot.background = element_rect(fill = "black"),
+    axis.title.x = element_text(color = "white"),
+    axis.title.y = element_text(color = "white")
+  )
+
+#----
+series_ids2 <- "NGDPRSAXDCGBQ"
+start_date2 <- "1955-01-01"
+end_date2 <- "2025-10-01"
+
+
+df2 <- fredo(api_key, series_ids2, start_date2, end_date2)
+
+ggplot(df2, aes(x = date, y = value)) + 
+  geom_line(color = "yellow", linewidth = 1) +
+  labs(
+    x = "Year",
+    y = "£ millions",
+    title = "UK Real GDP"
+  ) +
+  scale_y_continuous(labels = scales::comma) +
+  theme(
+    plot.title = element_text(size = 9, face = "bold", colour = "white"),
+    panel.background = element_rect(fill = "black"),
+    plot.background = element_rect(fill = "black"),
+    axis.title.x = element_text(color = "white"),
+    axis.title.y = element_text(color = "white")
+  )
+
+#----
+series_ids3 <- "LPRGDPUKA"
+start_date3 <- "1856-01-01"
+end_date3 <- "2016-01-01"
+
+
+df3 <- fredo(api_key, series_ids3, start_date3, end_date3)
+
+ggplot(df3, aes(x = date, y = value)) + 
+  geom_line(color = "yellow", linewidth = 1) +
+  labs(
+    x = "Year",
+    y = "Index: 2013 = 100",
+    title = "Real GDP Per Person in the United Kingdom"
+  ) +
+  scale_y_continuous(labels = scales::comma) +
+  theme(
+    plot.title = element_text(size = 9, face = "bold", colour = "white"),
+    panel.background = element_rect(fill = "black"),
+    plot.background = element_rect(fill = "black"),
+    axis.title.x = element_text(color = "white"),
+    axis.title.y = element_text(color = "white"), 
+    axis.text = element_text(color = "white")
+  )
+
+#----
+series_ids4 <- "DEBTTLGBA188A"
+start_date4 <- "1990-01-01"
+end_date4 <- "2024-01-01"
+
+
+df4 <- fredo(api_key, series_ids4, start_date4, end_date4)
+
+ggplot(df4, aes(x = date, y = value)) + 
+  geom_line(color = "yellow", linewidth = 1) +
+  labs(
+    x = "Year",
+    y = "% of GDP",
+    title = "UK Government Debt"
+  ) +
+  scale_y_continuous(labels = scales::comma) +
+  theme(
+    plot.title = element_text(size = 9, face = "bold", colour = "white"),
+    panel.background = element_rect(fill = "black"),
+    plot.background = element_rect(fill = "black"),
+    axis.title.x = element_text(color = "white"),
+    axis.title.y = element_text(color = "white"), 
+    axis.text = element_text(color = "white")
+  )
+
+#----
+series_ids5 <- "LFWA64TTGBQ647S"
+start_date5 <- "1971-01-01"
+end_date5 <- "2025-10-01"
+
+
+df5 <- fredo(api_key, series_ids5, start_date5, end_date5)
+
+ggplot(df5, aes(x = date, y = value)) + 
+  geom_line(color = "yellow", linewidth = 1) +
+  labs(
+    x = "Year",
+    y = "Count",
+    title = "UK Working Age Population - 1971 - 2025"
+  ) +
+  scale_y_continuous(labels = scales::comma) +
+  theme(
+    plot.title = element_text(size = 9, face = "bold", colour = "white"),
+    panel.background = element_rect(fill = "black"),
+    plot.background = element_rect(fill = "black"),
+    axis.title.x = element_text(color = "white"),
+    axis.title.y = element_text(color = "white"), 
+    axis.text = element_text(color = "white")
+  )
---
+++ FRED.md
@@ -0,0 +1,25 @@
+FRED
+================
+2026-03-30
+
+I recently downloaded a package from
+<https://fredblog.stlouisfed.org/2024/12/leveraging-r-for-powerful-data-analysis/>
+which lets you download FRED economic data straight into R Studio. I’m
+just testing it out here. ![](FRED_files/figure-gfm/plot-1.png)<!-- -->
+
+The above chart is from
+<https://fred.stlouisfed.org/series/NGDPRSAXDCGBQ>.
+
+![](FRED_files/figure-gfm/plot-2-1.png)<!-- -->
+
+The above chart is from <https://fred.stlouisfed.org/series/LPRGDPUKA>.
+
+![](FRED_files/figure-gfm/plot-3-1.png)<!-- -->
+
+The above chart is from
+<https://fred.stlouisfed.org/series/DEBTTLGBA188A>.
+
+![](FRED_files/figure-gfm/plot-4-1.png)<!-- -->
+
+The above chart is from
+<https://fred.stlouisfed.org/series/LFWA64TTGBQ647S>.
